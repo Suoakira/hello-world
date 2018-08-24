f624b06ad3a47cb4bf456b7a2405b820a48d8633
Stephen <37964234+Suoakira@users.noreply.github.com>
2018-08-24 17:35:29 +0100

    Add files via upload

---
+++ age in dog years.js
@@ -0,0 +1,18 @@
+const myAge = 25 // this is my age
+let earlyYears = 2 //this will change
+
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4; // dog years accounted for later one
+
+console.log(laterYears)
+console.log(earlyYears)
+
+const myAgeInDogYears = laterYears + earlyYears // calculating dog years
+
+const myName = "Suoarik".toLowerCase() // to lower case returns a string all lower case
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} old in dog years.`)
